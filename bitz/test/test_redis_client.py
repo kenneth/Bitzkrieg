f4497894600dcf68c13776395b1594f73672b514
Gavin Chan <gavincyi@gmail.com>
2017-04-06 06:56:44 +0800

    Updated mock test

--- bitz/test/test_redis_client.py
+++ bitz/test/test_redis_client.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3  
-import redis
 from bitz.redis_database_client import RedisDatabaseClient
-import mock
 import unittest
+from unittest.mock import patch, MagicMock
+
 
 class TestRedisDatabaseClient(unittest.TestCase):
-    
-    @mock.patch.object(redis.StrictRedis, 'set', autospec=True)
+    @patch('bitz.redis_database_client.redis.StrictRedis.set')
     def test_insert(self, mock_strictredis):
         client = RedisDatabaseClient()
         client.connect(host='localhost', 
                        port=9999,
                        db=3)
         client.insert('key1', 1)
-        mock_strictredis.assert_called_with(name='key1', value=1)
-        # self.assertTrue(client.insert('key2', 2))
-        
+        mock_strictredis.assert_called_with('key1', 1)
+
 if __name__ == '__main__':
     unittest.main()
