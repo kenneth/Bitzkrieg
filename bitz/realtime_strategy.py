4a79b125577b81bfc998ca0fdff84fc9c0f62a82
Gavin.Chan <gavincyi@gmail.com>
2017-04-28 08:19:08 +0800

    Resolved dependence issue #7

--- bitz/realtime_strategy.py
+++ bitz/realtime_strategy.py
@@ -12,7 +12,7 @@ class RealTimeStrategy:
     4. Maintain orders
     5. Risk management
     """
-    def __init__(self, name, ordsvr, logger):
+    def __init__(self, name, ordsvr, logger, max_fiat_currency_risk):
         """
         Constructor
         """
@@ -21,6 +21,7 @@ class RealTimeStrategy:
         self.logger = logger            # Logger
         self.market_data_feed = None    # Market data feed
         self.running = True
+        self.max_fiat_currency_risk = max_fiat_currency_risk
 
     def get_name(self):
         """
@@ -63,6 +64,11 @@ class RealTimeStrategy:
         """
         pass
 
+    def get_max_fiat_currency_risk(self):
+        """
+        Get maximum fiat currency risk
+        """
+        return self.max_fiat_currency_risk
 
 
 
